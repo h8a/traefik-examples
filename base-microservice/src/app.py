b3079d0cd8d3f80eee198ef7299e0dc414f0a77b
h8a <herii8a@gmail.com>
2023-03-29 23:52:35 -0700

    [ADD] example traefik middleware forwardauth

--- base-microservice/src/app.py
+++ base-microservice/src/app.py
@@ -1,5 +1,6 @@
 import falcon.asgi
 
+from resources.auth import AuthDenied, AuthSuccess
 from resources.message import MessageResource
 
 class Service(falcon.asgi.App):
@@ -8,3 +9,5 @@ class Service(falcon.asgi.App):
         super(Service, self).__init__()
 
         self.add_route('/api/v1.0.0/messages', MessageResource())
+        self.add_route('/api/v1.0.0/auth/denied', AuthDenied())
+        self.add_route('/api/v1.0.0/auth/success', AuthSuccess())
